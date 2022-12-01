09924c21e3bef249f5d09fa80ce519523aae90cf
Arun Thacharuthodi <67404902+arunthacharuthodi@users.noreply.github.com>
2022-12-01 14:28:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('aniuvind kk!');
+  res.send('it is the server root');
 });
-// app.get('/analytics', (req, res)=>{
-//     res.json({
-//         "voltage":19,
-//         "current":17,
-//         "reporting_time":"17 7 2022 IST"
-//     });
+app.get('/analytics', (req, res)=>{
+     res.json({
+         "voltage":19,
+         "current":17,
+         "reporting_time":"17 7 2022 IST
+  });
     
-// })
+});
+app.post("/duty_cycle", function(req, res) { 
+  var num1 = Number(req.body.duty); 
+  res.send("duty cycle changed successfully"); 
+}); 
 
-analytics()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
