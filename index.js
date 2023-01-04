fcbbd324aa929fdda781347d734fea3ee3402af4
arunthacharuthodi <arun@gmail.com>
2023-01-04 16:12:36 +0530

    backend working

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const express = require('express');
-const { default: analytics } = require('./api/analytics');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('it is the server root');
+  res.send('2000000000');
 });
 app.get('/analytics', (req, res)=>{
      res.json({
          "voltage":19,
          "current":17,
-         "reporting_time":"17 7 2022 IST
+         "reporting_time":"17 7 2022 IST"
   });
     
 });
-app.post("/duty_cycle", function(req, res) { 
-  var num1 = Number(req.body.duty); 
-  res.send("duty cycle changed successfully"); 
-}); 
+// app.post("/duty_cycle", function(req, res) { 
+//   var num1 = Number(req.body.duty); 
+//   res.send("duty cycle changed successfully"); 
+// }); 
 
 
 app.listen(port, () => {
